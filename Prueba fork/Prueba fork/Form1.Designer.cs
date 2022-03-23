17db91f91a8f55352464c9ecd434ebcc64d2da92
Sergio Carpena <s.carpena.j@gmail.com>
2022-03-23 23:26:10 +0100

    añadido lectura de vector, boton y funcion para devolver la media

--- Prueba fork/Prueba fork/Form1.Designer.cs
+++ Prueba fork/Prueba fork/Form1.Designer.cs
@@ -30,26 +30,51 @@ namespace Prueba_fork
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(290, 166);
+            this.button1.Location = new System.Drawing.Point(82, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 58);
+            this.button1.Size = new System.Drawing.Size(180, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Imprimir nombre";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 190);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comandos de prueba";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Introduce Valores al Vector";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(410, 246);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +82,8 @@ namespace Prueba_fork
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
